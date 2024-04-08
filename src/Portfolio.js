7478fa217c6610676b8f2bc707661c7e7470876e
MacMiniSuper <macminisuper@MacMiniSupers-Mac-mini-2.local>
2024-04-07 21:34:57 -0500

    Adds Portfolio placeholder

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,8 +1,6 @@
 const Portfolio = () => {
   return ( 
-    <div className="resume-viewer">
-      <img alt ='' src='./resume_page1.png' />
-    </div>
+    <div className="portfolio">Portfolio</div>
    );
 }
  
